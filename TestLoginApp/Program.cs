2643ee9672925c7f4128a984ac24ee00d4801ce1
Tim Miller <innerlogic4321@gmail.com>
2015-12-21 19:30:28 -0500

    Initial Commit

---
+++ TestLoginApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLoginApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Enter SA Username");
+            var username = Console.ReadLine();
+            Console.WriteLine("Enter SA Password");
+            var password = Console.ReadLine();
+            var result1 = await Test(username, password);
+            var cookies = result1.ToList();
+            Console.WriteLine("First Cookie Results: ");
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine("Cookie Name: " + cookie.Name);
+                Console.WriteLine("Cookie Domain: " + cookie.Domain);
+                Console.WriteLine("Cookie HttpOnly: " + cookie.HttpOnly);
+            }
+            var result2 = await Test(username, password);
+            cookies = result2.ToList();
+            Console.WriteLine("Second Cookie Results: ");
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine("Cookie Name: " + cookie.Name);
+                Console.WriteLine("Cookie Domain: " + cookie.Domain);
+                Console.WriteLine("Cookie HttpOnly: " + cookie.HttpOnly);
+            }
+            Console.WriteLine();
+        }
+
+        static async Task<CookieContainer> Test(string username, string password)
+        {
+            var cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                UseCookies = true,
+                UseDefaultCredentials = false,
+                CookieContainer = cookieContainer
+            };
+            using (var client = new HttpClient(handler))
+            {
+                var dic = new Dictionary<string, string>
+                {
+                    ["action"] = "login",
+                    ["username"] = username,
+                    ["password"] = password
+                };
+                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue()
+                {
+                    NoCache = true
+                };
+                var header = new FormUrlEncodedContent(dic);
+                var response = await client.PostAsync(new Uri("http://forums.somethingawful.com/account.php?"), header);
+                return cookieContainer;
+            }
+        }
+    }
+}
---
+++ TestSaUwp/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestSaUwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(UsernameTextbox.Text) || string.IsNullOrEmpty(PasswordTextBox.Password)) return;
+
+            var result = await Test(UsernameTextbox.Text, PasswordTextBox.Password);
+            var cookieList = Serialize(result.GetCookies(new Uri("http://fake.forums.somethingawful.com")));
+            ConsoleTest.Text = string.Empty;
+            ConsoleTest.Text = "First Cookie Result";
+
+            foreach (var cookie in cookieList)
+            {
+                ConsoleTest.Text += Environment.NewLine + "Cookie Name: " + cookie.Name;
+                ConsoleTest.Text += Environment.NewLine + "Cookie Domain: " + cookie.Domain;
+                ConsoleTest.Text += Environment.NewLine + "Cookie HttpOnly: " + cookie.HttpOnly;
+            }
+
+            result = await Test(UsernameTextbox.Text, PasswordTextBox.Password);
+            cookieList = Serialize(result.GetCookies(new Uri("http://fake.forums.somethingawful.com")));
+            ConsoleTest.Text += Environment.NewLine + "Second Cookie Result";
+
+            foreach (var cookie in cookieList)
+            {
+                ConsoleTest.Text += Environment.NewLine + "Cookie Name: " + cookie.Name;
+                ConsoleTest.Text += Environment.NewLine + "Cookie Domain: " + cookie.Domain;
+                ConsoleTest.Text += Environment.NewLine + "Cookie HttpOnly: " + cookie.HttpOnly;
+            }
+        }
+
+        List<Cookie> Serialize(CookieCollection cookies)
+        {
+            var cookieList = cookies.OfType<Cookie>();
+            return cookieList.ToList();
+        }
+
+        async Task<CookieContainer> Test(string username, string password)
+        {
+            var cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                UseCookies = true,
+                UseDefaultCredentials = false,
+                CookieContainer = cookieContainer
+            };
+            using (var client = new HttpClient(handler))
+            {
+                var dic = new Dictionary<string, string>
+                {
+                    ["action"] = "login",
+                    ["username"] = username,
+                    ["password"] = password
+                };
+                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue()
+                {
+                    NoCache = true
+                };
+                var header = new FormUrlEncodedContent(dic);
+                var response = await client.PostAsync(new Uri("http://forums.somethingawful.com/account.php?"), header);
+                return cookieContainer;
+            }
+        }
+    }
+}
